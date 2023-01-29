bb5ccf0a96c277ea8cd588f9e767e469f3e70cb5
Caleb Cleveringa <clever44@uw.edu>
2023-01-28 16:21:14 -0800

    ignore /build/

--- front-end/app/components/App.js
+++ front-end/app/components/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { render } from 'react-dom';
 
 function App() {
+    render()
     return (
       <div className="App">
         <header className="App-header">
-          <div>Hello Recipe Cart!</div>
+          <p>Hello Recipe Cart!</p>
         </header>
       </div>
     );
