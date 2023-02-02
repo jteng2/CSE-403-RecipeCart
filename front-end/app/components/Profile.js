fdd0ce99f45936d7742ca1e5984f8c2f5d7a1861
RickySin11 <69612398+SinghxRaj@users.noreply.github.com>
2023-02-02 15:29:00 -0800

    Create a profile drop down menu that display options. No menu option functionality.

--- front-end/app/components/Profile.js
+++ front-end/app/components/Profile.js
@@ -1,7 +1,50 @@
-import React from 'react'
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 function Profile() {
-    return (
-        <button>Profile</button>
-    );
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleOption = () => {
+    // TODO
+  }
+
+  return (
+    <div>
+      <Button
+        id="profile-button"
+        aria-controls={open ? 'profile-menu' : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? 'true' : undefined}
+        onClick={handleClick}
+      >
+        <AccountCircleIcon fontSize='large' color='action'/>
+      </Button>
+      <Menu
+        id="profile-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          'aria-labelledby': 'profile-button',
+        }}
+      >
+        <MenuItem onClick={handleOption}>My Account</MenuItem>
+        <MenuItem onClick={handleOption}>Settings</MenuItem>
+        <MenuItem onClick={handleOption}>Login</MenuItem>
+        <MenuItem onClick={handleOption}>Logout</MenuItem>
+      </Menu>
+    </div>
+  );
 }
+
+export default Profile;
