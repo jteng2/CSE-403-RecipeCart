9b1524979bc363a15be93715382d4645f24ed3b8
immen <chazi@uw.edu>
2023-02-03 00:51:34 -0800

    added SparkJava and Mongo dependencies

---
+++ src/main/java/com/gradle/tutorial/MongoExample.java
@@ -0,0 +1,14 @@
+package com.gradle.tutorial;
+
+import org.bson.Document;
+import com.mongodb.MongoException;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
+public class MongoExample {
+    public static void main(String[] args) {
+        System.out.println("imports good!");
+    }
+}
