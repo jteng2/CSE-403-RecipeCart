467185adf3f2370ed731a8731754ca1a56c2f80b
immen <chazi@uw.edu>
2023-02-06 03:57:11 -0800

    converted Result into a class that's to be overridden by EntityCommand's subclasses

--- src/main/java/usages/EntityCommand.java
+++ src/main/java/usages/EntityCommand.java
@@ -1,10 +1,6 @@
 /* (C)2023 */
 package usages;
 
-import entities.*;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import storage.EntityStorage;
@@ -34,139 +30,24 @@ public abstract class EntityCommand {
      */
     @NotNull abstract Result execute();
 
-    /** This class represents the immutable results from executing a EntityCommand. */
+    /** This class represents the results from executing a EntityCommand. */
     public static class Result {
         private final boolean success;
         private final @NotNull String message;
-        private final @NotNull List<Tag> tags;
-        private final @NotNull List<Ingredient> ingredients;
-        private final @NotNull List<Recipe> recipes;
-        private final @NotNull List<User> users;
 
-        private Result(
-                boolean success,
-                @NotNull String message,
-                @NotNull List<Tag> tags,
-                @NotNull List<Ingredient> ingredients,
-                @NotNull List<Recipe> recipes,
-                @NotNull List<User> users) {
+        Result(boolean success, @NotNull String message) {
             this.success = success;
             this.message = message;
-            this.tags = tags;
-            this.ingredients = ingredients;
-            this.recipes = recipes;
-            this.users = users;
         }
 
         /** @return true if the EntityCommand was executed successfully, false otherwise */
-        public boolean isSuccess() {
+        public boolean isSuccessful() {
             return success;
         }
 
         /** @return a message of if the execution was successful, or why it was unsuccessful */
         @NotNull public String getMessage() {
             return message;
-        }
-
-        /**
-         * @return an unmodifiable list with any Tags resulting from any querying this EntityCommand
-         *     did. If the EntityCommand did no querying, an empty list is returned.
-         */
-        @NotNull public List<Tag> getTags() {
-            return Collections.unmodifiableList(tags);
-        }
-
-        /**
-         * @return an unmodifiable list with any Ingredients resulting from any querying this
-         *     EntityCommand did. If the EntityCommand did no querying, an empty list is returned.
-         */
-        @NotNull public List<Ingredient> getIngredients() {
-            return Collections.unmodifiableList(ingredients);
-        }
-
-        /**
-         * @return an unmodifiable list with any Recipes resulting from any querying this
-         *     EntityCommand did. If the EntityCommand did no querying, an empty list is returned.
-         */
-        @NotNull public List<Recipe> getRecipes() {
-            return Collections.unmodifiableList(recipes);
-        }
-
-        /**
-         * @return an unmodifiable list with any Users resulting from any querying this
-         *     EntityCommand did. If the EntityCommand did no querying, an empty list is returned.
-         */
-        @NotNull public List<User> getUsers() {
-            return Collections.unmodifiableList(users);
-        }
-
-        /**
-         * This class is for incrementally building a Result field-by-field (instead of constructing
-         * a User with all fields at once). Not all fields need to be specified when building the
-         * Result. Unspecified data structure fields default to empty immutable data structures.
-         * "success" default to false. "message" defaults to an empty string.
-         */
-        static class Builder {
-            private boolean success;
-            private @NotNull String message;
-            private @NotNull List<Tag> tags;
-            private @NotNull List<Ingredient> ingredients;
-            private @NotNull List<Recipe> recipes;
-            private @NotNull List<User> users;
-
-            /** Initializes all fields to their defaults. */
-            Builder() {
-                this.success = false;
-                this.message = "";
-                this.tags = Collections.emptyList();
-                this.ingredients = Collections.emptyList();
-                this.recipes = Collections.emptyList();
-                this.users = Collections.emptyList();
-            }
-
-            /**
-             * @return a new Result with fields specified via this Builder. Modifying data
-             *     structures given to this Builder will not modify the returned Result.
-             */
-            Result build() {
-                return new Result(
-                        success,
-                        message,
-                        new ArrayList<>(tags),
-                        new ArrayList<>(ingredients),
-                        new ArrayList<>(recipes),
-                        new ArrayList<>(users));
-            }
-
-            Builder setSuccess(boolean success) {
-                this.success = success;
-                return this;
-            }
-
-            Builder setMessage(@NotNull String message) {
-                this.message = message;
-                return this;
-            }
-
-            Builder setTags(@NotNull List<Tag> tags) {
-                this.tags = tags;
-                return this;
-            }
-
-            Builder setIngredients(@NotNull List<Ingredient> ingredients) {
-                this.ingredients = ingredients;
-                return this;
-            }
-
-            Builder setRecipes(@NotNull List<Recipe> recipes) {
-                this.recipes = recipes;
-                return this;
-            }
-
-            Builder setUsers(@NotNull List<User> users) {
-                this.users = users;
-                return this;
-            }
         }
     }
 }
