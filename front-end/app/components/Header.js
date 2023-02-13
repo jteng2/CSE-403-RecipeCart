c724662708e41812f2e6cc834ebfa7b91b2cd19d
Caleb Cleveringa <clever44@uw.edu>
2023-02-13 15:32:47 -0800

    Fixed Header

--- front-end/app/components/Header.js
+++ front-end/app/components/Header.js
@@ -6,11 +6,8 @@ import Box from "@mui/material/Box";
 import ProfileButton from "./ProfileButton";
 import ShoppingListButton from "./ShoppingListButton";
 import SavedRecipeButton from "./SavedRecipeButton";
-<<<<<<< HEAD
 import SearchBar from "./SearchBar";
-=======
 import LogoButton from "./LogoButton";
->>>>>>> main
 
 function Header() {
     return (
@@ -21,22 +18,8 @@ function Header() {
                         <ShoppingListButton />
                         <SavedRecipeButton />
                     </Grid>
-<<<<<<< HEAD
-                    <Grid item xs={3}>
-                        <Box
-                            sx={{
-                                width: 100,
-                                height: 50,
-                                backgroundColor: "#33E3EC",
-                                borderRadius: "6px"
-                            }}
-                        >
-                            <Typography color="black">Recipe Cart</Typography>
-                        </Box>
-=======
                     <Grid item xs="3">
                         <LogoButton />
->>>>>>> main
                     </Grid>
                     <Grid item xs={3}>
                         <SearchBar />
