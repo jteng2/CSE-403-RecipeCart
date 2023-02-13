394a60de37bd45dd21eeb4dd4ce84b316e3463a8
Rajvir Singh <69612398+SinghxRaj@users.noreply.github.com>
2023-02-13 14:26:48 -0800

    Home page recipe entries list

---
+++ front-end/app/components/RecipeEntry.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./../resources/styles/RecipeEntry.css";
+
+function RecipeEntry() {
+    return (
+        <div className="recipe-entry">
+            Recipe
+        </div>
+    );
+}
+
+export default RecipeEntry;
--- front-end/app/components/pages/Home.js
+++ front-end/app/components/pages/Home.js
@@ -1,11 +1,15 @@
 import React from "react";
 import Header from "../Header";
+import RecipeEntryList from "../RecipeEntryList";
 
 function Home() {
     return(
         <div>
             <p>HOME PAGE</p>
             <Header />
+            <RecipeEntryList />
         </div>
     );
-} export default Home;
+}
+
+export default Home;
