41da74f79fc05c8a4988465266122fd85b51e234
RickySin11 <69612398+SinghxRaj@users.noreply.github.com>
2023-02-13 17:54:43 -0800

    Renamed RecipeEntry to RecipeSmallBox.

---
+++ front-end/app/components/RecipeSmallBox.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./../resources/styles/RecipeSmallBox.css";
+
+function RecipeSmallBox() {
+    return (
+        <div className="recipe-box">
+            Recipe
+        </div>
+    );
+}
+
+export default RecipeSmallBox;
