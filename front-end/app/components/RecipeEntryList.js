1dc5dfd2f760f53ebb08316490bfb0f2e06e5a50
Rajvir Singh <69612398+SinghxRaj@users.noreply.github.com>
2023-02-13 18:53:30 -0800

    Recipe dialogue component

--- front-end/app/components/RecipeEntryList.js
+++ front-end/app/components/RecipeEntryList.js
@@ -2,34 +2,25 @@ import React from 'react';
 import RecipeEntry from './RecipeEntry';
 import Grid from "@mui/material/Grid";
 import "./../resources/styles/RecipeEntryList.css";
+import RecipeSmallBox from "./RecipeSmallBox";
+
+const number = [1, 3, 5];
 
 function RecipeEntryList() {
     return (
         <Grid container alignItems="center">
-            <Grid container justifyContent="center" spacing={2} className="row">
-                <Grid item>
-                    <RecipeEntry />
+            {number.map(i => {
+                return (
+                <Grid container justifyContent="center" spacing={2} className="row">
+                    <Grid item>
+                        <RecipeEntry recipeNumber={i} component={<RecipeSmallBox />}/>
+                    </Grid>
+                    <Grid item>
+                        <RecipeEntry recipeNumber={i+1} component={<RecipeSmallBox />}/>
+                    </Grid>
                 </Grid>
-                <Grid item>
-                    <RecipeEntry />
-                </Grid>
-            </Grid>
-            <Grid container justifyContent="center" spacing={2} className="row">
-                <Grid item>
-                    <RecipeEntry />
-                </Grid>
-                <Grid item>
-                    <RecipeEntry />
-                </Grid>
-            </Grid>
-            <Grid container justifyContent="center" spacing={2} className="row">
-                <Grid item>
-                    <RecipeEntry />
-                </Grid>
-                <Grid item>
-                    <RecipeEntry />
-                </Grid>
-            </Grid>
+                )
+            })}
         </Grid>
     );
 }
