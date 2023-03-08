a6635ce0eb73199060b0beaa9b575d5a1a694847
Caleb Cleveringa <clever44@uw.edu>
2023-03-07 20:47:15 -0800

    Added commas to directions

--- front-end/app/components/RecipeEntry.js
+++ front-end/app/components/RecipeEntry.js
@@ -78,7 +78,7 @@ export default function RecipeEntry(props) {
               "No ingredients"}
           </Typography>
           <Typography gutterBottom>
-            Directions: {props.directions ? props.directions : "No directions"}
+            Directions: {props.directions ? props.directions.join(", ") : "No directions"}
           </Typography>
           <Typography gutterBottom>
             Prep: {props.time_to_cook ? props.time_to_cook + " minutes" : "20 minutes"}          </Typography>
