d6adbbb2c3cdb1bcedfb785d045267303604951b
RickySin11 <rajvir11singh@outlook.com>
2023-03-07 20:02:36 -0800

    Calls API to find recipe search results that are displayed

--- front-end/app/components/RecipeSearchList.js
+++ front-end/app/components/RecipeSearchList.js
@@ -1,63 +1,52 @@
 import React from "react";
+import { useState, useEffect } from 'react';
 import SearchBar from "./SearchBar";
 import RecipeEntry from "./RecipeEntry";
 import RecipeBigTile from "./RecipeBigTile";
 import "../resources/styles/RecipeSearchList.css"
 
-async function get_recipes(searchQuery) {
-    if (!searchQuery) {
-        return { "matches": [] }
-    }
-    searchQuery = searchQuery.replace(' ', '+').toLowerCase();
-    let url = `http://localhost:4567/search/recipes?terms=${searchQuery}`;
-    try {
-        let response = await fetch(url, {
-            mode: 'cors'
-        });
-        return await response.json();
-    } catch (error) {
-        console.error(error);
-        return {
-            error: error.message
-        };
-    }
-}
 
-async function RecipeSearchList(props) {
+
+
+function RecipeSearchList(props) {
+    const [matching, setMatching] = useState([]);
+
     const populated = "Showing search results for: \"";
-    let search = props?.search ?? "";
-    let updateSearch = props?.updateSearch ?? "";
-    async function find_recipes() {
-        let search = props?.search;
-        const recipes_data = await get_recipes(search);
-        let matching_recipes = recipes_data["matches"];
 
-        console.log(matching_recipes);
-        let recipe_entries = []
-        for (let i = 0; i < matching_recipes.length; i++) {
-            const name = matching_recipes[i]["name"];
-            const ingredients = matching_recipes[i]["ingredients"];
-            const time_to_cook = matching_recipes[i]["time to cook"];
-            recipe_entries[i] = <RecipeEntry key={name}
-                recipeNumber={name}
-                ingredients={ingredients}
-                time_to_cook={time_to_cook}
-                component={<RecipeBigTile name={name} />} />;
-        }
-        return recipe_entries;
-    };
+    useEffect(() => {
+        fetch(`http://localhost:4567/search/recipes?terms=${props.search}`)
+            .then(res => res.json())
+            .then((result) => setMatching(result["matches"]));
+    }, []);
 
-    let found_recipes = await find_recipes();
+
+    let recipe_components = []
+    for (let i = 0; i < matching.length; i++) {
+        let recipe_name = matching[i]["name"];
+        let presentation_name = matching[i]["presentationName"];
+        let time_to_cook = matching[i]["cookTime"];
+        // let ingredients = matching[i]["requiredIngredients"];
+        let directions = matching[i]["directions"].join(", ");
+
+        recipe_components[i] = <RecipeEntry key={recipe_name}
+            recipeName={presentation_name}
+            time_to_cook={time_to_cook}
+            component={<RecipeBigTile name={presentation_name} />}
+            directions={directions}
+        />
+    }
+
+
     return (
         <div className="search">
-            <SearchBar updateSearch={updateSearch} />
-            <p>{search ?
-                populated + search + "\"" :
+            <SearchBar updateSearch={props.updateSearch} />
+            <p>{props.search ?
+                populated + props.search + "\"" :
                 "Enter a recipe to search"}
             </p>
-            {search ?
+            {props.search ?
                 <ul className="recipe-list">
-                    {found_recipes.map((recipeItem, index) => (
+                    {recipe_components.map((recipeItem, index) => (
                         <li className="recipe-item" key={index}>{recipeItem}</li>
                     ))}
                 </ul> : null}
