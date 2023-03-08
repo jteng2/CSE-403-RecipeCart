24b220587f4789fc754d050ba7e4b73bb3c18201
Caleb Cleveringa <101697161+fawful514@users.noreply.github.com>
2023-03-07 17:26:38 -0800

    User authentication (#44)
    * User Login successful
    
    * User profile page
    
    * Removed unused var
    
    * Disabled old test

--- front-end/app/components/pages/Profile.js
+++ front-end/app/components/pages/Profile.js
@@ -1,8 +1,11 @@
-import React from "react";
+import React, {useState} from "react";
 import Grid from "@mui/material/Grid";
 import Header from "../Header";
+import ProfileInfo from "../ProfileInfo";
+import { useAuth0 } from "@auth0/auth0-react";
 
 function Profile() {
+    const { user, isAuthenticated } = useAuth0();
     return(
         <Grid container>
             <Grid container alignItems="center" justifyContent="center" direction="column">
@@ -12,6 +15,7 @@ function Profile() {
                 <Grid item>
                     <Header />
                 </Grid>
+                {isAuthenticated ? <ProfileInfo user={user}/> : <h1>Please Sign in to view Profile</h1>}
             </Grid>
         </Grid>
     );
--- front-end/app/index.js
+++ front-end/app/index.js
@@ -1,12 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
+import { Auth0Provider } from "@auth0/auth0-react";
 import App from "./components/App";
 
 ReactDOM.render(
     <React.StrictMode>
       <BrowserRouter>
-        <App />
+        <Auth0Provider
+          domain="dev-pwa58nltvew2lltr.us.auth0.com"
+          clientId="vJ6ZeIlgkE9U9DnZ32bhNLLyFCNKfN59"
+          redirectUri={window.location.origin}>
+          <App />
+        </Auth0Provider>
       </BrowserRouter>
     </React.StrictMode>,
     document.getElementById("root")
